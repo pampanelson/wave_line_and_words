f8ebe04789af6ea5c65ed1102b3e9a08eb6ab228
Pampa Nelson <pampa.nelson@gmail.com>
2019-08-24 10:49:10 +0800

    reset need change place

--- source/plugins/AddSubtract/AddSubtract.cpp
+++ source/plugins/AddSubtract/AddSubtract.cpp
@@ -15,14 +15,14 @@
 
 static CFFGLPluginInfo PluginInfo ( 
 	AddSubtract::CreateInstance,		// Create method
-	"PWL0_Ripple_Line",								// Plugin unique ID
-	"Ripple Line",					// Plugin name
+	"1_6_temp_need_change",								// Plugin unique ID
+	"Ripple1_6_temp_need_change",					// Plugin name
 	1,						   			// API major version number 													
 	500,								// API minor version number
 	1,									// Plugin major version number
 	000,								// Plugin minor version number
 	FF_EFFECT,							// Plugin type
-	"Ripple lines",			// Plugin description
+	"1_6_temp_need_change",			// Plugin description
 	"by Pampa -- lohosoft.com"				// About
 );
 
