0bd84977498f1ac1c2b9768b1c90ff765be28af7
Pampa Nelson <pampa.nelson@gmail.com>
2019-08-25 17:24:19 +0800

    Update Frag.h

--- source/plugins/AddSubtract/Frag.h
+++ source/plugins/AddSubtract/Frag.h
@@ -21,28 +21,10 @@ vec2 fragCoord;
 // uniform float switchTex;
 
     
-uniform float bLineRipple;
-uniform float bLineTracking;
-    
-
-uniform float lineNum;
-uniform float lineWidth;
-uniform float lineOffset;
-uniform float lineMiRippleSize;
-uniform float lineMiRippleSpeed;
-uniform float lineMaRippleSize;
-uniform float lineMaRippleSpeed;
-    
-    
 uniform float trk1Angle;
-uniform float trk1Power;
-    
 uniform float trk2Angle;
-uniform float trk2Power;
-
 uniform float trk3Angle;
-uniform float trk3Power;
-
+uniform float distortScale;
 
                                                         
                                                         
@@ -60,106 +42,6 @@ float smax(float a,float b,float k){
     return smin(a,b,-k);
 }
 
-float wave1(float x){
-    return x;
-}
-float wave2(float x,float amplitude,float freq){
-    float res = 0.0;
-
-    res = amplitude - pow(x*freq,2.0);
-    return res;
-}
-// peak is highest point , narrow is wave wide
-float wave3(float x,float peak,float narrow){
-    float res = 0.0;
-    res = peak - narrow*abs(x*x*x);
-    return res;
-}
-
-
-
-float wave_distort(float use,vec2 st,float angle){
-    // distort =========================================
-
-    
-    vec2 st2 = st;
-    //st.x = st.x/(PI*2.0) + .5; // before st.x is -π ~ π after is  normalized 0.0 ~ 1.0
-
-    float x = st2.x;
-    x *= .2; // wave smooth factor
-    // x -= fract(iTime*0.1);
-    //x += 0.5;
-    //x = -x;
-
-    // =+++++++++++++ IMPORTANT ++++++++++++++++++++++++++++
-    // not use distort or distort angle is 0.0 which is default value
-    if(use < 0.5){
-         
-       x += 1.; // this value makes no distort ================ . TODO 
-    }
-    else{
-        x += -.0; // 0.0 is up direct , -0.5 is right direct , 0.5 is left direct
-        x += 0.5 - angle;// angle is 0. ~ 1, 1 is right direct , 0 is left direct 
-
-    }
-    
-    // float x = uv.x;
-    float y = 0.0;
-    float a1 = -.2*sin(iTime*5.0);
-    float f1 = 12.5;
-    float y1 = wave2(x,a1,f1);
-    float a2 = 0.0;//
-    a2 = sin(iTime*10.)*0.1;
-    float f2 = 18.0; // power ===============================
-    float y2 = wave2(x+0.1,a2,f2);
-    y = smax(y,y1,0.9);
-    y = smax(y,y2,0.8);
-    // y = smax(y,wave1(x*0.01),-0.9);
-    float peak3 = 0.15;//
-    float narrow3 = 4.0;//*sin(iTime*10.);
-    float y3 = wave3(x+0.2,peak3,narrow3);
-
-    y = smax(y,y3,0.8);
-    y = smax(y,0.2,0.9);
-
-    y *= .8;// whole scale =======================
-
-    return y;
-}
-
-
-float line_wave(vec2 st,float lineNum,float lineWidth,float lineOffset,float distort,float bLineRipple){
-    float line;
-    float sty = st.y;
-    if(bLineRipple > 0.5){
-        float maRipple = sin(iTime) * lineMaRippleSpeed;
-        //line = 1. - smoothstep(0.0,lineWidth,abs(sty + ripple - 0.1 ));
-            
-        sty += maRipple * lineMaRippleSize;
-    }
-        
-    sty /= distort;
-    sty *= lineNum;
-    
-    
-    float index = floor(sty);
-    if(index > lineOffset){
-        
-        if(bLineRipple > 0.5){
-            float miRipple = sin(index*iTime) * lineMiRippleSpeed;
-            
-            sty += miRipple * lineMiRippleSize;
-        }
-        
-        sty = fract(sty);
-    
-        line = 1. - smoothstep(0.0,lineWidth,abs(sty - 0.1));
-    
-
-    }
-
-    return line;
-}
 
 void main()
 {
@@ -172,44 +54,12 @@ void main()
     fragCoord = vec2(gl_FragCoord.x,iResolution.y - gl_FragCoord.y);
     
     // --------------------------
-
-
-    vec2 uv = (fragCoord.xy - .5 * iResolution.xy)/iResolution.x; // uv -.5 ~ .5  , x axis is scale t0 1.
-
-    
-    // prepare uv for st **********************************
-    uv *= 2.0; // -1. ~ 1.
-    uv.y += iResolution.y/iResolution.x;// origin point on (0.5 * x , 0.0)
-
-    // uv.y -= wordOffset;// whole screen offset ======================
-    // uv *= 0.5;// 0 ~ 1
-    
-    vec2 st = vec2(atan(uv.x,uv.y),length(uv));
-    //st.x += PI;// 0 ~ 2PI on -y axis 
-
-    
-    float y = wave_distort(bLineTracking,st,trk1Angle);
-    y += wave_distort(bLineTracking,st,trk2Angle);
-    y += wave_distort(bLineTracking,st,trk3Angle);
-
-
     vec3 col;
-
-
-    col = vec3(line_wave(st,lineNum,lineWidth,lineOffset,y,bLineRipple));
     
     
+
     
     
-    // debug distort wave =============
-     bool bWaveDistortDebug = true;
-//     if(st.y < y && bWaveDistortDebug){
-//         col = vec3(1.0);
-//     }
-
-    // Output to screen
-
-//    col = vec3(sin(iTime));
     fragColor = vec4(col,1.0);
     
     gl_FragColor = fragColor;
