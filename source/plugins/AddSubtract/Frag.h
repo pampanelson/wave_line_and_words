aa3488f0bc2e15ed20b23d2aafd3ab993e08654e
Pampa Nelson <pampa.nelson@gmail.com>
2019-08-25 19:30:15 +0800

    distort domain need work

--- source/plugins/AddSubtract/Frag.h
+++ source/plugins/AddSubtract/Frag.h
@@ -43,6 +43,18 @@ float smax(float a,float b,float k){
 }
 
 
+float distort(vec2 st,float angle){
+    angle *= 0.5;
+    angle += 0.25;
+
+    float r;
+    float peakSharp = 0.3;// 0.3~ 0.4
+    r = 1. - pow(abs(st.x-angle),peakSharp);
+    r = smoothstep(0.0,0.77,r);
+    r *= 1. + distortScale;
+    return r;
+}
+
 void main()
 {
     
@@ -51,14 +63,49 @@ void main()
     vec4 fragColor;
     iTime = ticks/1000.0;
     iResolution = vec2(width,height);
-    fragCoord = vec2(gl_FragCoord.x,iResolution.y - gl_FragCoord.y);
+
+    vec2 fragCoord1 = vec2(gl_FragCoord.x,iResolution.y - gl_FragCoord.y);
     
+    vec2 uv = gl_FragCoord.xy / iResolution.xy;// --------------  default uv
     // --------------------------
     vec3 col;
     
-    
 
     
+
+   vec2 uv1 = (fragCoord1.xy - .5 * iResolution.xy)/iResolution.y; // uv -.5 ~ .5
+   uv1 *= 2.0; // -1. ~ 1.
+   uv1.y += 1.0;
+
+
+   vec2 st = vec2(atan(uv1.x,uv1.y),length(uv1));
+
+   st.x = st.x/(PI*2.0) + .5; // before st.x is -π ~ π after is  normalized 0.0 ~ 1.0
+
+
+   float d = distort(st,trk1Angle);
+   d += distort(st,trk2Angle);
+   d += distort(st,trk3Angle);
+
+
+   if(st.y < d){
+
+//        col += 1.0;
+   }
+   float thetaX = d * cos(st.x);
+   float thetaY = d * sin(st.x);
+
+
+
+   uv.x = uv.x + thetaX;
+   uv.y = uv.y + thetaY;
+
+
+    
+    // for debug default uv domain of input texture------------------- 
+    //fragColor = vec4(uv.x,uv.y,0.0,.5);
+
+    col = texture2D(inputTexture,uv).xyz;
     
     fragColor = vec4(col,1.0);
     
