3939f7a22696cf5f7b816fc52b217df1e0341028
Pampa Nelson <pampa.nelson@gmail.com>
2019-09-13 14:55:39 +0800

    update

--- source/plugins/AddSubtract/Frag.h
+++ source/plugins/AddSubtract/Frag.h
@@ -49,6 +49,185 @@ float aPI = acos(-1.);
 
 
 
+
+
+float rand(float n){return fract(sin(n) * 43758.5453123);}
+
+                                                    
+// Polynomial smooth min (for copying and pasting into your shaders)
+float smin(float a, float b, float k) {
+        float h = clamp(0.5 + 0.5*(a-b)/k, 0.0, 1.0);
+    return mix(a, b, h) - k*h*(1.0-h);
+}
+
+float smax(float a,float b,float k){
+    return smin(a,b,-k);
+}
+
+float wave1(float x){
+    return x;
+}
+float wave2(float x,float amplitude,float freq){
+    float res = 0.0;
+
+    res = amplitude - pow(x*freq,2.0);
+    return res;
+}
+// peak is highest point , narrow is wave wide
+float wave3(float x,float peak,float narrow){
+    float res = 0.0;
+    res = peak - narrow*abs(x*x*x);
+    return res;
+}
+
+float wave_distort1(float use,vec2 st,float angle,float scale){
+    // distort =========================================
+    
+    float fixScale = 0.5;
+    vec2 st2 = st;
+    //st.x = st.x/(PI*2.0) + .5; // before st.x is -π ~ π after is  normalized 0.0 ~ 1.0
+    
+    float x = st2.x;
+    x *= .2; // wave smooth factor
+    // x -= fract(iTime*0.1);
+    //x += 0.5;
+    //x = -x;
+    
+    // =+++++++++++++ IMPORTANT ++++++++++++++++++++++++++++
+    // not use distort or distort angle is 0.0 which is default value
+    if(use < 0.5){
+        
+        x += 1.; // this value makes no distort ================ . TODO
+    }
+    else{
+        x += -.0; // 0.0 is up direct , -0.5 is right direct , 0.5 is left direct
+        x += 0.5 - angle;// angle is 0. ~ 1, 1 is right direct , 0 is left direct
+        
+    }
+    
+    // float x = uv.x;
+    float y = 0.0;
+    // float a1 = -.2*sin(iTime*5.0);
+    // a1 = 0.8; // use this one to control wave shape, 0.05 is small 0.8 is biggest
+    float a1 = scale;
+    a1 += sin(iTime)*0.02;// add some dynamics
+    float f1 = 12.5;
+    float y1 = wave2(x,a1,f1);
+    float a2 = 0.0;//
+    // a2 = sin(iTime*10.)*0.1;
+    a2 = 0.1;
+    float f2 = 8.0; // power ===============================
+    float y2 = wave2(x+0.1,a2,f2);
+    y = smax(y,y1,0.9);
+    y = smax(y,y2,0.8);
+    // y = smax(y,wave1(x*0.01),-0.9);
+    float peak3 = 0.15;//
+    float narrow3 = 4.0;//*sin(iTime*10.);
+    float y3 = wave3(x+0.2,peak3,narrow3);
+    
+    y = smax(y,y3,0.5);// 0.1 is sharper wave , ============  TODO
+    y = smax(y,0.2,fixScale);// scale is 0.01 ~ 0.9 , lower is bigger ========  TODO
+    
+    y *= 1.;// whole scale =======================
+    y += sin(iTime) * 0.01; // shaking
+    
+    return y;
+}
+
+
+vec3 word_wave(vec2 st,float rotateSpeed,float distort,float colNumber,float offsetY,float lsratio,float wsratio){
+    
+    float speed = rotateSpeed;// sign means rotate clock(+) or counterclock(-)
+    float piFactor = 128.0;// bigger number will avoid jump on loop but no effect on rotate speed
+    float m = mod(speed*iTime,piFactor * PI);
+    
+    // make distort
+    
+    if(distort > 0.0){
+        st.y /= distort;// how ??????????????? divided a number less 1.0 will amplify
+    }
+    
+    
+    
+    //float colNumber = 10.; //   input parameters ------------------
+    st.y *= colNumber;
+    
+    
+    //st1.y = max(4.0,st1.y);
+    
+    //float offsetY = 7.0; // pass parameters
+    
+    if(st.y < offsetY){
+        return vec3(0.0);
+    }
+    st.x *= 1.*floor(st.y);
+    
+    if(bWordRotate > 0.5){
+        
+        if(mod(floor(st.y),2.0)>0.0){
+            st.x += m;
+        }else{
+            st.x -= m;
+        }
+        
+        
+    }
+    
+    
+    vec2 index = floor(st);
+    vec2 polar = fract(st);
+    
+    vec3 col;
+    // Time varying pixel color
+    //col = vec3(polar.x,polar.y,0.0);
+    
+    vec2 localPolar = polar;
+    
+    
+    // pass parameter
+    localPolar.x *= 1.0/(1.0 - wsratio);
+    localPolar.y *= 1.0/(1.0 - lsratio);
+    
+    
+    
+    
+    
+    if(localPolar.x > 1.0 || localPolar.y > 1.0 || localPolar.y < 0.0 || localPolar.x < 0.0){
+        col *= 0.0;
+    }
+    
+    else{
+        
+        
+        // try to get part of texture but whole in a word square
+        // ratio of part of texture is fixed, column line number is changing , need get module of fixed word column amount predefined
+        // devided texture into 4 parts for example
+        // -=--------------------------------------------------- =====================  IMPORTANT ==================
+        // how many column and line a texture of words
+        
+        float yDivid = wordColDivid;// passed uniform float
+        float indexY = mod(index.y,yDivid);
+        localPolar.y = indexY * 1./yDivid + localPolar.y / yDivid;
+        
+        float xDivid = wordWordDivid;// passed uniform float
+        float indexX = mod(index.x,xDivid);
+        localPolar.x = indexX * 1./xDivid + localPolar.x / xDivid;
+        
+        
+        // convert back to original left upper 0,0, at end of all =====================  IMPORTANT ==================
+        localPolar.y = 1. - localPolar.y;
+        // maybe need flip x axis also 
+//        localPolar.x = 1. - localPolar.x;
+        col = texture2D(inputTexture,localPolar).bbb;
+        
+    }
+    
+    
+    return col;
+    
+}
+
+
 void main()
 {
     
@@ -83,41 +262,67 @@ void main()
     float sty = fragCoord.y / iResolution.y;// 0 ~ 1
     float stx = fragCoord.x / iResolution.x;// 0~1
     
-    
+    bool tracked = false;
+
+
     vec3 col;
 
     vec2 uv1 = fragCoord.xy/iResolution.xy;
-    float st1y = uv1.y;
+    float st1y = uv1.y; // keep original y position for read texture later 
+
     uv1.y *= iResolution.y/iResolution.x;
 
-    col = texture2D(inputTexture,vec2(uv1.x,1.-st1y)).rgb;
+
+
+    // how to read original position info =========================  IMPORTANT=+++++++++++++++++++ 
+    // col += texture2D(inputTexture,vec2(uv1.x,1.-st1y)).rgb * 0.3;
 
     float angle = stTrackAngleOffset;
     float r = stTrackRadius;
-    vec2 center = vec2(0.5,0.);
+    vec2 center;
     center = vec2(stTrackOriginX,stTrackOriginY);
     
-    float x3 = center.x - cos(angle*PI)*r;
-    float y3 = center.y + sin(angle*PI)*r;
+    // float x3 = center.x - cos(angle*PI)*r;
+    // float y3 = center.y + sin(angle*PI)*r;
 
-    if(length(uv1-vec2(x3,y3))< 0.1){
-        vec2 readst = vec2(uv1.x,1.-st1y);
-        col += texture2D(inputTexture,readst).rgb*0.8;
 
-        vec3 texCol = col;
 
-        if(col.r < 0.8 && length(uv1-vec2(x3,y3))< 0.01){
+    float dist = length(uv1-center);
 
-            col = vec3(0.,1.,.0);
+
+    if(dist > stTrackRadius - 0.01 && dist < stTrackRadius + 0.01){
+        vec3 texInfo = texture2D(inputTexture,vec2(uv1.x,1.-st1y)).rgb;
+        if (texInfo.r < 0.3 && texInfo.r > 0.0) // remove black part of text data
+            {
+                col = vec3(0.,1.,1.0);
+                tracked = true;
+            }
+
+    }
+
+
+    float distort;
+
+    if(bWordTracking > 0.0){
+        if(tracked){
+            float scale = 0.2;
+            float angle = st.x;
+            distort = 0.2;
+            col = word_wave(st,wordRotateSpeed,distort,wordLineNum,wordOffset,wordLineSpacingRatio,wordWordSpacingRatio);
 
         }
 
-        
-    }else{
-        //col += texture2D(inputTexture,uv).rgb*0.2;
-
     }
+
+
+    // show anchor point of detect circle 
     
+
+
+    col += word_wave(st,wordRotateSpeed,0.0,wordLineNum,wordOffset,wordLineSpacingRatio,wordWordSpacingRatio);
+
+
+
     if(length(uv1-center)< 0.03){
         col.r += 1.;
     }
